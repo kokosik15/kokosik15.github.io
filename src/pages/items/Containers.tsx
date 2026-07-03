1f91d291ccedab81f1186c0fbb294cc2aaad7a35
kokosik15 <kovalikmax56@gmail.com>
2026-07-04 02:44:40 +0300

    Update API endpoint for fetching items

--- src/pages/items/Containers.tsx
+++ src/pages/items/Containers.tsx
@@ -38,7 +38,7 @@ export function ContainersPage() {
       try {
         setLoading(true);
         const response = await axios.get<ApiResponse>(
-          `http://127.0.0.1:5000/items/${category}`,
+          `https://stalcraft-wiki-api-github-io.onrender.com/items/${category}`,
         );
         setItems(response.data.items);
         setError(null);
