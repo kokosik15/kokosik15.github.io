872f61b92baaa947f98ce8764170a1aa7242df74
kokosik15 <kovalikmax56@gmail.com>
2026-07-04 02:40:52 +0300

    Update ItemsPage.tsx

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -14,7 +14,7 @@ export function ItemsPage() {
   useEffect(() => {
     const fetchItemsData = async () => {
       try {
-        const response = await axios.get("https://stalcraft-wiki-api-github-io.onrender.com", {
+        const response = await axios.get("https://stalcraft-wiki-api-github-io.onrender.com/counts", {
           timeout: 5000,
           headers: { Accept: "application/json" },
         });
